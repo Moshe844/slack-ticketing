f6c7816f93233d0857042de0b5358bcf552cafda
Moshe Ekstein <mekstein@fidelitypayment.com>
2023-11-29 13:50:52 -0500

    changed blocks

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = new App({
 
 app.command('/ticket_request', async({ack, body, client})=> {
      await ack();
-     console.log(body);
+     console.log(body.trigger_id);
      try{
         const result = await client.views.open({
             trigger_id: body.trigger_id,
@@ -35,180 +35,33 @@ app.command('/ticket_request', async({ack, body, client})=> {
                 },
                 "title": {
                     "type": "plain_text",
-                    "text": "Workplace check-in",
+                    "text": "Submit ticket",
                     "emoji": true
                 },
                 "blocks": [
                     {
-                        "type": "section",
-                        "text": {
-                            "type": "plain_text",
-                            "text": ":wave: Hey David!\n\nWe'd love to hear from you how we can make this place the best place you’ve ever worked.",
-                            "emoji": true
-                        }
-                    },
-                    {
-                        "type": "divider"
-                    },
-                    {
                         "type": "input",
-                        "label": {
-                            "type": "plain_text",
-                            "text": "You enjoy working here at Pistachio & Co",
-                            "emoji": true
-                        },
                         "element": {
-                            "type": "radio_buttons",
-                            "options": [
-                                {
-                                    "text": {
-                                        "type": "plain_text",
-                                        "text": "Strongly agree",
-                                        "emoji": true
-                                    },
-                                    "value": "1"
-                                },
-                                {
-                                    "text": {
-                                        "type": "plain_text",
-                                        "text": "Agree",
-                                        "emoji": true
-                                    },
-                                    "value": "2"
-                                },
-                                {
-                                    "text": {
-                                        "type": "plain_text",
-                                        "text": "Neither agree nor disagree",
-                                        "emoji": true
-                                    },
-                                    "value": "3"
-                                },
-                                {
-                                    "text": {
-                                        "type": "plain_text",
-                                        "text": "Disagree",
-                                        "emoji": true
-                                    },
-                                    "value": "4"
-                                },
-                                {
-                                    "text": {
-                                        "type": "plain_text",
-                                        "text": "Strongly disagree",
-                                        "emoji": true
-                                    },
-                                    "value": "5"
-                                }
-                            ]
-                        }
-                    },
-                    {
-                        "type": "input",
-                        "label": {
-                            "type": "plain_text",
-                            "text": "What do you want for our team weekly lunch?",
-                            "emoji": true
+                            "type": "plain_text_input",
+                            "action_id": "plain_text_input-action"
                         },
-                        "element": {
-                            "type": "multi_static_select",
-                            "placeholder": {
-                                "type": "plain_text",
-                                "text": "Select your favorites",
-                                "emoji": true
-                            },
-                            "options": [
-                                {
-                                    "text": {
-                                        "type": "plain_text",
-                                        "text": ":pizza: Pizza",
-                                        "emoji": true
-                                    },
-                                    "value": "value-0"
-                                },
-                                {
-                                    "text": {
-                                        "type": "plain_text",
-                                        "text": ":fried_shrimp: Thai food",
-                                        "emoji": true
-                                    },
-                                    "value": "value-1"
-                                },
-                                {
-                                    "text": {
-                                        "type": "plain_text",
-                                        "text": ":desert_island: Hawaiian",
-                                        "emoji": true
-                                    },
-                                    "value": "value-2"
-                                },
-                                {
-                                    "text": {
-                                        "type": "plain_text",
-                                        "text": ":meat_on_bone: Texas BBQ",
-                                        "emoji": true
-                                    },
-                                    "value": "value-3"
-                                },
-                                {
-                                    "text": {
-                                        "type": "plain_text",
-                                        "text": ":hamburger: Burger",
-                                        "emoji": true
-                                    },
-                                    "value": "value-4"
-                                },
-                                {
-                                    "text": {
-                                        "type": "plain_text",
-                                        "text": ":taco: Tacos",
-                                        "emoji": true
-                                    },
-                                    "value": "value-5"
-                                },
-                                {
-                                    "text": {
-                                        "type": "plain_text",
-                                        "text": ":green_salad: Salad",
-                                        "emoji": true
-                                    },
-                                    "value": "value-6"
-                                },
-                                {
-                                    "text": {
-                                        "type": "plain_text",
-                                        "text": ":stew: Indian",
-                                        "emoji": true
-                                    },
-                                    "value": "value-7"
-                                }
-                            ]
+                        "label": {
+                            "type": "plain_text",
+                            "text": "Subject",
+                            "emoji": true
                         }
                     },
                     {
                         "type": "input",
                         "label": {
                             "type": "plain_text",
-                            "text": "What can we do to improve your experience working here?",
+                            "text": "How can we help you?",
                             "emoji": true
                         },
                         "element": {
                             "type": "plain_text_input",
                             "multiline": true
                         }
-                    },
-                    {
-                        "type": "input",
-                        "label": {
-                            "type": "plain_text",
-                            "text": "Anything else you want to tell us?",
-                            "emoji": true
-                        },
-                        "element": {
-                            "type": "plain_text_input",
-                            "multiline": true
-                        },
-                        "optional": true
                     }
                 ]
             }

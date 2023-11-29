c0755e1d712276a682f77a273f084129622742df
Moshe Ekstein <mekstein@fidelitypayment.com>
2023-11-29 12:17:31 -0500

    Added secret  commit

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = new App({
     signingSecret: process.env.SLACK_SIGNING_SECRET,
     token: process.env.SLACK_BOT_TOKEN,
 });
+console.log('SLACK_SIGNING_SECRET:', process.env.SLACK_SIGNING_SECRET);
 
 (async () => {
     await app.start(process.env.PORT || 3000);

7da1058730c94b87d3fa2f531dd8345b2a994162
Moshe Ekstein <mekstein@fidelitypayment.com>
2023-11-30 12:02:24 -0500

    added email

--- server.js
+++ server.js
@@ -80,18 +80,37 @@ app.command('/ticket_request', async({ack, body, client})=> {
         console.error(error)
      }
 })
-
-app.view('your_view_callback_id', async ({ ack, body }) => {
-    // await sendEmail(client, body.view.state.values);
-    await ack();
-    console.log('Acknowledged view submission', body);
+app.view('your_view_callback_id', async ({ ack, body, client }) => {
+    try {
+        // Process the view submission here
+        await sendEmail(client, body.view.state.values);
+        await ack();
+        console.log('Acknowledged view submission', body);
+    } catch (error) {
+        console.error('Error processing view submission:', error);
+        await ack({ response_action: 'errors', errors: { error: 'Something went wrong.' } });
+    }
 });
 
-// const transporter = nodemailer.createTransport({
-//     host: "smtp-mail.outlook.com",
-//     port: 993,
-//     secure: true,
-//     auth: {
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+       user: process.env.GMAIL_USER,
+       pass: process.env.GMAIL_PASSWORD
+    }
+})
 
-//     }
-// })
+async function sendEmail(){
+    const info = await transporter.sendMail({
+        from: process.env.GMAIL_USER,
+        to: "arye6700@gmail.com",
+        subject: "Hello",
+        text: "testing purposes"
+    });
+
+    console.log("Message sent %s", info.messageId);
+}
+
+sendEmail().catch(console.error)

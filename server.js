a644cebec4dc716cb6491cc661b6df8210c67523
Moshe Ekstein <mekstein@fidelitypayment.com>
2023-12-04 16:30:12 -0500

    added automated /

--- server.js
+++ server.js
@@ -9,26 +9,21 @@ const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
 });
 // console.log('SLACK_SIGNING_SECRET:', process.env.SLACK_SIGNING_SECRET);
+const channelID = 'CDV45R12B';
+const commandMessage = 'To submit a ticket, use the command `/ticket-request`.';
 
-const sendCommendMessage = async (channelID, text, user) => {
-    try {
-        await app.client.chat.postEphemeral({
-            channel: channelID,
-            text: text,
-            user: user
+app.event('app_home_opened', async({event, client})=> {
+    if(event.channel === channelID){
+        await client.chat.postEphemeral({
+            channel: event.user,
+            text: commandMessage
         })
-    } catch (error) {
-        console.error('Error sending message:', error);
     }
-};
+})
+
 
 
 (async () => {
-    const channelID = 'CDV45R12B';
-    const userWhoOpenedChanel = body.user.id
-    const commandMessage = 'To submit a ticket, use the command `/ticket-request`.';
-
-    await sendCommendMessage(channelID, userWhoOpenedChanel, commandMessage)
     await app.start(process.env.PORT || 3000);
     console.log('⚡️ Bolt app is running!');
 })()
